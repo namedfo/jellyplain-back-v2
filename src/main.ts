5f2d417ec4730cb203c3526b5c31db61c7b606c4
Misha Poleshchenkov <74454444+MRXOneX@users.noreply.github.com>
2022-11-04 13:55:26 +0200

    update

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+//
+import * as bodyParser from 'body-parser';
+//
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  await app.listen(parseInt(process.env.PORT) || 3333);
+}
+bootstrap();
