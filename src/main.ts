780d8be0cc085aaf26f94f401f79c56004a1fc84
mrxonex <mikhail.poleshchenkov@mail.ru>
2022-11-06 18:36:03 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,25 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const whitelist = [
+    'http://localhost:3000',
+    'https://jellyplainv2.herokuapp.com',
+  ];
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log('allowed cors for:', origin);
+        callback(null, true);
+      } else {
+        console.log('blocked cors for:', origin);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    credentials: true,
+  });
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(parseInt(process.env.PORT) || 3333);
