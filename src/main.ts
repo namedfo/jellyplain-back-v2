1d0856505903cd6fbf1069b3eea2ba3abfd376ca
mrxonex <mikhail.poleshchenkov@mail.ru>
2022-11-06 18:22:34 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as bodyParser from 'body-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
+  const app = await NestFactory.create(AppModule);
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(parseInt(process.env.PORT) || 3333);
