b1cc2a9dc8edc1272ce3170b032b1ddf04d2247c
mrxonex <mikhail.poleshchenkov@mail.ru>
2022-11-12 18:15:28 +0300

    updatge

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -31,34 +31,35 @@ export class AuthService {
   }
 
   async vk(body: any) {
-    const vkUserData: any = await this.getUserDataFromVk(body.id, body.token);
+    return "ee"
+    // const vkUserData: any = await this.getUserDataFromVk(body.id, body.token);
 
-    const _user = await this.prisma.user.findUnique({
-      where: {
-        vkID: vkUserData.id,
-      },
-    });
-    if (_user) {
-      return {
-        ..._user,
-        token: this.jwtService.sign({ id: _user.id }),
-      };
-    }
-    if (vkUserData.id && !_user) {
-      const newUser = await this.prisma.user.create({
-        data: {
-          vkID: vkUserData.id,
-          first_name: vkUserData.first_name,
-          last_name: vkUserData.last_name,
-          avatar_url: vkUserData.image,
-          bdate: vkUserData.bdate,
-        },
-      });
-      return {
-        ...newUser,
-        token: this.jwtService.sign({ id: newUser.id }),
-      };
-    }
+    // const _user = await this.prisma.user.findUnique({
+    //   where: {
+    //     vkID: vkUserData.id,
+    //   },
+    // });
+    // if (_user) {
+    //   return {
+    //     ..._user,
+    //     token: this.jwtService.sign({ id: _user.id }),
+    //   };
+    // }
+    // if (vkUserData.id && !_user) {
+    //   const newUser = await this.prisma.user.create({
+    //     data: {
+    //       vkID: vkUserData.id,
+    //       first_name: vkUserData.first_name,
+    //       last_name: vkUserData.last_name,
+    //       avatar_url: vkUserData.image,
+    //       bdate: vkUserData.bdate,
+    //     },
+    //   });
+    //   return {
+    //     ...newUser,
+    //     token: this.jwtService.sign({ id: newUser.id }),
+    //   };
+    // }
   }
 
   async getUserDataFromVk(userId: number, token: string) {
