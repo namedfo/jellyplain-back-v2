abfd103ef57548dffb66f813d2a297a1193978a4
mrxonex <mikhail.poleshchenkov@mail.ru>
2022-11-13 10:32:59 +0300

    u

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -75,20 +75,30 @@ export class AuthService {
     //   image: data.data.response[0]?.photo_200,
     //   bdate: data.data.response[0]?.bdate,
     // };
+    // return {
+    //   id: 2,
+    //   createdAt: '2022-11-12T14:28:19.356Z',
+    //   updatedAt: '2022-11-12T14:28:19.356Z',
+    //   vkID: 286453384,
+    //   role: 'default',
+    //   bdate: '28.6.2005',
+    //   first_name: 'Миша',
+    //   last_name: 'Полещенков',
+    //   avatar_url:
+    //     'https://sun1.userapi.com/sun1-84/s/v1/ig2/rDRmDR9Fd9HKxnwviOE1UQ3krBOX6TG8_Ivbn2BlS9XDA_iltzzg0E2uZqULWZ17v-sTIUmKX8QuIhXZeWSkceXm.jpg?size=200x200&quality=96&crop=0,16,200,200&ava=1',
+    //   token:
+    //     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjY4MzI0NDc0LCJleHAiOjE2Njg0MTA4NzR9.zcYQYUahsYd5yVk1jsc4wVD805Ymy8r44nGs54lyg2s',
+    // };
+    // }
     return {
-      id: 2,
-      createdAt: '2022-11-12T14:28:19.356Z',
-      updatedAt: '2022-11-12T14:28:19.356Z',
-      vkID: 286453384,
-      role: 'default',
+      id: 286453384,
       bdate: '28.6.2005',
+      photo_200:
+        'https://sun1.userapi.com/sun1-84/s/v1/ig2/rDRmDR9Fd9HKxnwviOE1UQ3krBOX6TG8_Ivbn2BlS9XDA_iltzzg0E2uZqULWZ17v-sTIUmKX8QuIhXZeWSkceXm.jpg?size=200x200&quality=96&crop=0,16,200,200&ava=1',
       first_name: 'Миша',
       last_name: 'Полещенков',
-      avatar_url:
-        'https://sun1.userapi.com/sun1-84/s/v1/ig2/rDRmDR9Fd9HKxnwviOE1UQ3krBOX6TG8_Ivbn2BlS9XDA_iltzzg0E2uZqULWZ17v-sTIUmKX8QuIhXZeWSkceXm.jpg?size=200x200&quality=96&crop=0,16,200,200&ava=1',
-      token:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjY4MzI0NDc0LCJleHAiOjE2Njg0MTA4NzR9.zcYQYUahsYd5yVk1jsc4wVD805Ymy8r44nGs54lyg2s',
+      can_access_closed: true,
+      is_closed: false,
     };
-    // }
   }
 }
