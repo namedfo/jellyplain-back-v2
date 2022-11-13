cb1aa176cc1677d0e8c1392ae9924d0370317223
mrxonex <mikhail.poleshchenkov@mail.ru>
2022-11-13 17:55:04 +0300

    s

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -51,6 +51,14 @@ export class ProductService {
       where: {
         id: +query.id,
       },
+      include: {
+        productChilds: {
+          include: {
+            images: true,
+          },
+        },
+        reviews: true,
+      },
     });
   }
 }
